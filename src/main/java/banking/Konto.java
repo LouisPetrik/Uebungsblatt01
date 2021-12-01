8dd2aa2831b5985ec4b96fb53ea79bf250f22302
LouisPetrik <33809479+LouisPetrik@users.noreply.github.com>
2021-12-01 17:30:25 +0100

    jetzt noch die sessions richtig machen

---
+++ src/main/java/banking/Konto.java
@@ -0,0 +1,14 @@
+package banking;
+
+public class Konto {
+	public String kontoname; 
+	public String besitzerEmail; 
+	public Integer ID; 
+	
+	public Konto(String kontoname, String besitzerEmail, Integer ID) {
+		this.kontoname = kontoname; 
+		this.besitzerEmail = besitzerEmail; 
+		this.ID = ID; 
+	}
+	
+}
