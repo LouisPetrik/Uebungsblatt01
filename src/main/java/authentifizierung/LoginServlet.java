4b40560e9c2cf2d2bf8358b0236e7f638297aaba
LouisPetrik <33809479+LouisPetrik@users.noreply.github.com>
2021-12-02 15:14:32 +0100

    anmeldung fängt jetzt ab, wenn es überhaupt keine registrierten nutzer gibt. Als nöchstes versuche ich, dass die kontenliste sich wirklich aktulaisiert

--- src/main/java/authentifizierung/LoginServlet.java
+++ src/main/java/authentifizierung/LoginServlet.java
@@ -30,44 +30,25 @@ public class LoginServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+    
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-
+    /* 
+     * 	ACHTUNG: DIESE METHODE IST EXPERIMENTEL und soll verhindern, dass beim neuladen der LoginServlet Seite 
+     * die Daten verloren gehen - was leider ziemlich nervig ist. 
+     */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-		 */
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		String email = request.getParameter("email"); 
-		String passwort = request.getParameter("passwort");
+		// abfangen, wenn jemand die Konto.jsp neuladen will, weil die URL dazu tatsächlih 
+		// die LoginServlet ist, und nicht /konto.jsp
 		
-		System.out.println(email); 
-		System.out.println(passwort); 
 		
-		// aus der session die kunden in eine arrayliste laden: 
 		HttpSession session = request.getSession(); 
 		// Das scheint irgendwie ekelig zu sein, klappt aber. 
 		ArrayList<Kunde> kundenliste = (ArrayList<Kunde>) session.getAttribute("bank.kundenliste"); 
 		
-		
-		System.out.println(kundenliste.get(0)); 
-		
-		// Falls auf den zustand noch mehrfach zugegriffen werden muss. 
-		Boolean eingeloggt = false; 
-
-		
-		// checken, ob nutzer wirklich registiert ist: 
 		for (Kunde kunde : kundenliste) {
-			if (kunde.email.equals(email) && kunde.passwort.equals(passwort)) {
-				eingeloggt = true; 
-					
+	
+	
+		
 				// Vor und Nachname des erfolgreich eingeloggten Kunden zur Begrüßung übergeben 
 				request.setAttribute("vorname", kunde.vorname);
 				request.setAttribute("nachname", kunde.nachname); 
@@ -96,16 +77,89 @@ public class LoginServlet extends HttpServlet {
 				request.getRequestDispatcher("konto.jsp").forward(request, response);
 			} 
 		}
+
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+
+		 */
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String email = request.getParameter("email"); 
+		String passwort = request.getParameter("passwort");
 		
-		if (!eingeloggt) {
-			System.out.println("Email oder passwort ist falsch"); 
-			request.setAttribute("fehlertyp", "E-Mail oder Passwort ist falsch"); 
+		System.out.println(email); 
+		System.out.println(passwort); 
+		
+		// aus der session die kunden in eine arrayliste laden: 
+		HttpSession session = request.getSession(); 
+		// Das scheint irgendwie ekelig zu sein, klappt aber. 
+		ArrayList<Kunde> kundenliste = (ArrayList<Kunde>) session.getAttribute("bank.kundenliste"); 
+		
+		
+		// Falls auf den zustand noch mehrfach zugegriffen werden muss. 
+		Boolean eingeloggt = false; 
+
+		
+		// Falls die Kundenliste leer ist, und somit kein Benutzer registiert ist, 
+		// können wir uns das natürlich alles sparen. 
+		if (!(kundenliste == null)) {
+			// ab hier alles für den Fall, dass es überhaupt Kunden in der DB gibt
+			
+			// checken, ob nutzer wirklich registiert ist: 
+			for (Kunde kunde : kundenliste) {
+				if (kunde.email.equals(email) && kunde.passwort.equals(passwort)) {
+					eingeloggt = true; 
+						
+					// Vor und Nachname des erfolgreich eingeloggten Kunden zur Begrüßung übergeben 
+					request.setAttribute("vorname", kunde.vorname);
+					request.setAttribute("nachname", kunde.nachname); 
+					request.setAttribute("email", kunde.email); 
+					
+					// Session für den angemeldeten Kunden anlegen. 
+					// setzt die email des aktuell angemeldeten Kunden als Session wert
+					// Die email weil vor und nachname nicht einzigartig sein müssen. 
+					session.setAttribute("bank.email", kunde.email); 
+					
+					
+					// übergeben einer Liste an Konten zur Kontrolle: 
+					// dafür nur eine Liste von Namen der Konten anlegen: 
+					ArrayList<String> kontennamenListe = new ArrayList<String>(); 
+					
+					for (Konto konto : kunde.kontenliste) {
+						kontennamenListe.add(konto.kontoname); 
+					}
+					
+					session.setAttribute("bank.kontennamenListe", kontennamenListe); 
+					
+					System.out.println("Liste von Konten: " + kontennamenListe); 
+					
+					request.setAttribute("kontennamenListe", kontennamenListe); 
+					
+					request.getRequestDispatcher("konto.jsp").forward(request, response);
+				} 
+			}
+			
+			if (!eingeloggt) {
+				System.out.println("Email oder passwort ist falsch"); 
+				request.setAttribute("fehlertyp", "E-Mail oder Passwort ist falsch"); 
+				
+				request.getRequestDispatcher("login.jsp").forward(request, response); 
+			} 
+			
+		// Falls es überhaupt keine Kunden gibt
+		} else {
+			System.out.println("Keine Kudnen registriert"); 
+			request.setAttribute("fehlertyp", "Es gibt keine registrierten Kunden"); 
 			
 			request.getRequestDispatcher("login.jsp").forward(request, response); 
-		} 
+		}
 		
-		
-		//doGet(request, response);
+
 	}
 
 }
--- src/main/webapp/konto.jsp
+++ src/main/webapp/konto.jsp
@@ -5,29 +5,38 @@
 	<jsp:include page="head.jsp" />
 <body>
 	<jsp:include page="navigation.jsp" />
-	<p>Hallo! ${ vorname } ${ nachname }</p>
-	<p>Ihre email über session: ${ sessionScope['bank.email'] }</p>
+	
+	<div class="container">
+		<h1>Hallo ${ vorname } ${ nachname }!</h1>
+		
+		<div class="card">
+		  <div class="card-body">
+		   	Sie sind angemeldet als: ${ sessionScope['bank.email'] }
+		  </div>
+		</div>
+		
+		<h2>Hier können sie weitere Konten anlegen:</h2>
+		
+		<p>Liste der konten in der session ${ sessionScope['bank.kontennamenListe'] }</p>
+		
+		
+		<form method="POST" action="KontoServlet">
+			<input type="text" name="kontoname" placeholder="Kontoname" /> 
+			<input type="submit" value="Konto anlegen" /> 
+		</form>
+		
+		<form method="POST" action="LogoutServlet">
+			<input type="submit" value="Ausloggen" />
+			<!--  Das Form sendet ebenfalls die email des nutzers, der sich abmelden will, damit 
+			er darüber identifiziert und abgemeldet werden kann. Die Email deshalb, weil sie im Gegensatz 
+			zum Namen eines Kunden einzigartig sein MUSS -->
+			<input type="hidden" name="email" value="${ email }" />
+		</form>
+		
+		<h2>Kontostand.xls Datei hochladen</h2>
+	</div>
 	
 	
-	<p>Hier können Sie ein Konto anlegen:</p>
-	
-	<p>Liste der konten in der session ${ sessionScope['bank.kontennamenListe'] }</p>
-	
-	
-	<form method="POST" action="KontoServlet">
-		<input type="text" name="kontoname" placeholder="Kontoname" /> 
-		<input type="submit" value="Konto anlegen" /> 
-	</form>
-	
-	<form method="POST" action="LogoutServlet">
-		<input type="submit" value="Ausloggen" />
-		<!--  Das Form sendet ebenfalls die email des nutzers, der sich abmelden will, damit 
-		er darüber identifiziert und abgemeldet werden kann. Die Email deshalb, weil sie im Gegensatz 
-		zum Namen eines Kunden einzigartig sein MUSS -->
-		<input type="hidden" name="email" value="${ email }" />
-	</form>
-	
-	<h2>Kontostand.xls Datei hochladen</h2>
 
 	<jsp:include page="footer.jsp" />
 </body>
