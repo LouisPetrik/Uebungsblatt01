b4789eee05d13937177d8439a91ea22ac22b2786
LouisPetrik <33809479+LouisPetrik@users.noreply.github.com>
2021-12-03 17:00:51 +0100

    habe die Validierung.java hinzugefügt, die das validieren von Form eingaben wie in der registrierung und login übernimmt. Die idee habe ich notiert, und die klasse sollte möglichst wiederverwendbar sein

---
+++ src/main/java/authentifizierung/Validierung.java
@@ -0,0 +1,14 @@
+package authentifizierung;
+
+// Diese Klasse dient dazu, Nutzereingaben in Formularen zu valideren. 
+
+
+// die klasse geht über alle bedingungen, fügt die erfüllten einem 
+// array hinzu, und wenn etwas fehlt wird geschmissen, was fehlt. Außerdem werden die existierenden werte 
+// dann an das template zurückgegeben, also wenn z. B. vorname fehlt, dass nachname, email und co. ausgefüllt bleiben. 
+// Sollte wiederverwendbar sein, damit sie für die 3 forms: Konten, Login und Registrierung genutzt werden können. 
+
+
+public class Validierung {
+
+}
